817b9abc2366c9c2459dad74a6761e16ae62ddc6
isalevine <isaacdlevine@gmail.com>
2019-04-12 11:10:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+array = [1, 2, 3, 25]
+
+function countEm(array) {
+  return array.reduce(addValues, 0)
+}
+
+function addValues(acc, val) {
+  return acc + val
+}
+
+let totalBatteries = countEm(array)
